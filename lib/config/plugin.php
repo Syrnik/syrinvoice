31658d30a8a001c15f09077d78370bd0852fce2b
Serge Rodovnichenko <sergerod@gmail.com>
2019-02-01 15:55:12 +0300

    Version changed in config

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @package Syrinvoice.Config
- * @version 2.2.0
- * @copyright (c) 2014-2016, Serge Rodovnichenko
+ * @version 3.0.0
+ * @copyright (c) 2014-2019, Serge Rodovnichenko
  * @license http://www.webasyst.com/terms/#eula Webasyst
  */
 
@@ -10,7 +10,7 @@ return array(
     'name'        => _wp('Invoice'),
     'description' => _wp('Simple and clean Invoice printform'),
     'vendor'      => 670917,
-    'version'     => '2.2.0',
+    'version'     => '3.0.0',
     'img'         => 'img/invoice-icon.png',
     'frontend'    => false,
     'printform'   => true,
