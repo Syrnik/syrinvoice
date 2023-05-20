b8b5d9d7e1080e8783867016bae9c9bbca0e0a52
Serge Rodovnichenko <sergerod@gmail.com>
2023-05-20 15:42:06 +0300

    Исправлены поля с номером телефона для фреймворка 2.8+

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -8,7 +8,7 @@ return [
     'name'        => /*_wp*/('Invoice'),
     'description' => /*_wp*/('Simple and clean Invoice printform'),
     'vendor'      => 670917,
-    'version'     => '4.1.0',
+    'version'     => '4.1.1',
     'img'         => 'img/invoice-icon.png',
     'frontend'    => false,
     'printform'   => true,
